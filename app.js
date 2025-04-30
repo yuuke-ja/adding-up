a077ac2debcbda11df008a8d51e389fdaf64a935
中尾優介 <youjiezhongwei2@gmail.com>
2025-04-30 16:20:25 +0900

    ランキングのじっそう

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 'use strict';
+const fs = require('node:fs');
+const readline = require('node:readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: rs });
+const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+rl.on('line', lineString => {
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2016 || year === 2021) {
+    let value = null;
+    if (prefectureDataMap.has(prefecture)) {
+      value = prefectureDataMap.get(prefecture);
+    } else {
+      value = {
+        before: 0,
+        after: 0,
+        change: null
+      };
+    }
+    if (year === 2016) {
+      value.before = popu;
+    }
+    if (year === 2021) {
+      value.after = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
+  }
+});
+rl.on('close', () => {
+    for (const [ke,value]of prefectureDataMap){
+        value.change=value.after/value.before;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair2[1].change - pair1[1].change;
+      });
+      console.log(rankingArray);
+})
